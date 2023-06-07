f0440b10b388935663484c751da11b4282860706
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-06-07 19:07:00 +0300

    finished ui design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log("hello from js")
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=8a02c5cf19fb71dea03ff61ab5cea91f
+//https://api.openweathermap.org/data/2.5/weather?q=${*cityName*}&appid=${*API_key*}&units=metric
